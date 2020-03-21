938bc71cc5202c82bfe7b550322a820a1c194d66
Chris felts <echristianfelts@gmail.com>
2020-03-21 04:50:08 -0700

    Initial Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // get horiz input
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput *= speed;
+        // define move based on input
+        moveDirection = new Vector3(horizontalInput, 0.0f, 0.0f);
+
+        //  move in that dir
+
+        _controller.Move(moveDirection *Time.deltaTime);
+
+
+
+    }
+}
